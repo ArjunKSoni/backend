cab2d867f166e5e8b43cd05a292855fd428871ca
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-03-25 11:55:02 +0530

    commit

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express= require('express')
 const app=express()
-var cors = require('cors')
+// var cors = require('cors')
 const port=3000
 require("./connect_mongo")
 
 // session creation   npm i express-session
-var session=require("express-session")
+// var session=require("express-session")
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+// app.use(express.urlencoded({extended:true}))
 app.use('/api/auth',require('./routes/auth'))
 // app.use('/api/store',require('./routes/store'))
+app.get("/",(req,res)=>{
+    res.send("api is live")
+})
 
 
 
 app.listen(port,()=>{
-    console.log(`successfully loaded at ${port}`)
+    console.log(`successfully loaded at`)
 })
 
 module.exports=app;
